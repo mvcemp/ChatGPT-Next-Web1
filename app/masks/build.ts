1fd0e31be6d22683cac8870d4678fd0beaf0c798
codencoffee <96663459+mvcemp@users.noreply.github.com>
2024-11-09 23:38:21 +0000

    Disable masks

--- app/masks/build.ts
+++ app/masks/build.ts
@@ -13,13 +13,3 @@ const BUILTIN_MASKS: Record<string, BuiltinMask[]> = {
 };
 
 const dirname = path.dirname(__filename);
-
-fs.writeFile(
-  dirname + "/../../public/masks.json",
-  JSON.stringify(BUILTIN_MASKS, null, 4),
-  function (error) {
-    if (error) {
-      console.error("[Build] failed to build masks", error);
-    }
-  },
-);
