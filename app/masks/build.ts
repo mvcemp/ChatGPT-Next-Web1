ec0b298f827e0cbfef8559865f6b0425083279f5
codencoffee <96663459+mvcemp@users.noreply.github.com>
2024-11-09 23:39:03 +0000

    Remove masks

--- app/masks/build.ts
+++ app/masks/build.ts
@@ -11,15 +11,3 @@ const BUILTIN_MASKS: Record<string, BuiltinMask[]> = {
   tw: TW_MASKS,
   en: EN_MASKS,
 };
-
-const dirname = path.dirname(__filename);
-
-fs.writeFile(
-  dirname + "/../../public/masks.json",
-  JSON.stringify(BUILTIN_MASKS, null, 4),
-  function (error) {
-    if (error) {
-      console.error("[Build] failed to build masks", error);
-    }
-  },
-);
